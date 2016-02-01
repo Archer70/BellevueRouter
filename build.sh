ebcb92e7f8ce73623523ab0d721a16c6fe4c5dee
Archer70 <code.archer70@gmail.com>
2016-02-01 01:57:15 -0800

    Travis integration.

---
+++ build.sh
@@ -0,0 +1,6 @@
+# Install the latest Composer because sometimes Travis' is out of date.
+curl -sS https://getcomposer.org/installer | php
+php composer.phar install;
+
+# Run the tests.
+vendor/bin/phpunit --bootstrap test/bootstrap.php
