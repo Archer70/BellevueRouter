4831e0f640b391b4acf32747e1a91f6b067b2eed
Archer70 <code.archer70@gmail.com>
2016-02-02 15:07:17 -0800

    Add PHPUnit Dependency

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 # Install the latest Composer because sometimes Travis' is out of date.
 curl -sS https://getcomposer.org/installer | php
 php composer.phar install;
 
 # Run the tests.
-vendor/bin/phpunit --bootstrap test/bootstrap.php
+vendor/bin/phpunit --bootstrap test/bootstrap.php test
